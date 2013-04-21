30d37856b42bede6ca54fd26191b4691a7721472
Matt Charters <matt@well.ca>
2013-04-21 16:30:04 -0400

    Oh, of course. This standard method only works if you put it *before* everything else. And don't bother to document it, that's cool.

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+// db connection
+app.use(function(req, res, next) {
+	req.mongodb = mongodb;
+	next();
+});
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -34,20 +41,14 @@ app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// db connection
-var loadMongo = function(req, res, next) {
-	req.mongodb = mongodb;
-	next();
-};
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
-app.get('/players', loadMongo, players.findAll);
-app.get('/teams', loadMongo, teams.findAll);
+app.get('/players', players.findAll);
+app.get('/teams', teams.findAll);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
